cb09542813406cc5bb5769df011cac65072c94ef
msanfimiadi-cloud <ms.anfimiadi@gmail.com>
2026-08-26 14:40:25 +0700

    Add Unity prototype scene creator

---
+++ Assets/IZMI/Editor/CreatePrototypeScene.cs
@@ -0,0 +1,29 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+namespace Izmi.Editor
+{
+    public static class CreatePrototypeScene
+    {
+        [MenuItem("IZMI/Create Prototype Scene")]
+        public static void Create()
+        {
+            var scene = EditorSceneManager.NewScene(
+                NewSceneSetup.EmptyScene,
+                NewSceneMode.Single);
+
+            EditorSceneManager.SaveScene(
+                scene,
+                "Assets/IZMI/Scenes/GlobePrototype.unity");
+
+            EditorBuildSettings.scenes = new[]
+            {
+                new EditorBuildSettingsScene(
+                    "Assets/IZMI/Scenes/GlobePrototype.unity",
+                    true)
+            };
+        }
+    }
+}
